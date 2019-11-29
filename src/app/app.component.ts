b9f3bfa189119d3495a645d8b6131b56d4904d2d
Stepan S. Sickar <mkoty@yandex.ru>
2019-11-29 18:26:52 +0700

    init project; added first page simple design;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'After';
+export class AppComponent implements AfterViewInit {
+  showGradient = false;
+  showAfterLabel = false;
+
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.showGradient = true;
+    }, 1000);
+
+    setTimeout(() => {
+      this.showAfterLabel = true;
+    }, 2000);
+  }
 }
